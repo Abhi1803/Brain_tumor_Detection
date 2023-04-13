27e2e496e991138a5916e23deac125b771d353d8
Abhinav Srivatssan <92371009+Abhi1803@users.noreply.github.com>
2023-04-13 13:32:39 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -26,7 +26,7 @@ st.caption("Warning: Do not click Recognize button before uploading image. It wi
 
 # Load the model
 
-model = load_model("Model.h5")
+model = load_model("Brain_Tumor_Image_Classification_Model(2).h5")
 
 
 
